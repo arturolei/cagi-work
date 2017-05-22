474744af96e39d79a4a7123d65243a37959a7364
arturolei <artie.lei@gmail.com>
2017-05-21 21:30:12 -0700

    Files prepped for final run.

--- csv-generator.py
+++ csv-generator.py
@@ -15,16 +15,16 @@ import csv
 with open ('5-Hopkins_clinical_panel_submission_template.txt', 'r') as my_file:
     column_headings = my_file.read().split(',')
      
-print (column_headings, len(column_headings))
-print (column_headings, type(column_headings))
+#print (column_headings, len(column_headings))
+#print (column_headings, type(column_headings))
 
 rows_list = ['P'+ str(patient_num+1) for patient_num in range(106)]
 blank_entries = ['*' for num in column_headings]
-print(blank_entries)
+#print(blank_entries)
 
 starter_dict  = {key:value for key,value in zip(column_headings, blank_entries)}
 
-print(starter_dict)
+#print(starter_dict)
 
 dict_list = []
 
@@ -33,10 +33,8 @@ dict_list = []
 for item in range(106):
     temp_dict = {'patient': 'P'+str(item+1), ' 1-P': '*', '1-SD': '*', '1-V': '*', ' 2-P': '*', '2-SD': '*', '2-V': '*', ' 3-P': '*', '3-SD': '*', '3-V': '*', '4-P': '*', '4-SD': '*', '4-V': '*', '5-P': '*', '5-SD': '*', '5-V': '*', '6-P': '*', '6-SD': '*', '6-V': '*', '7-P': '*', '7-SD': '*', '7-V': '*', '8-P': '*', ' 8-SD': '*', ' 8-V': '*', ' 9-P': '*', ' 9-SD': '*', ' 9-V': '*', ' 10-P': '*', '10-SD': '*', '10-V': '*', '11-P': '*', '11-SD': '*', ' 11-V': '*', ' 12-P': '*', '12-SD': '*', ' 12-V': '*', '13-P': '*', '13-SD': '*', '13-V': '*', ' 14-P': '*', '14-SD': '*', ' 14-V': '*', 'C': '*'}
     dict_list.append(temp_dict)
-    
-print (dict_list)
-    
-print (dict_list)
+
+#print (dict_list)
 
 csv_keys = dict_list[0].keys()
 
